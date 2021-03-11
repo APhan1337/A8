c708fac74b9f57d0ac4c863e88f40c10cbdfe856
Alex Phan <33383909+APhan1337@users.noreply.github.com>
2021-03-11 00:09:04 -0800

    Complete journey page

--- public/js/journey.js
+++ public/js/journey.js
@@ -5,7 +5,46 @@ $(document).ready(function () {
 })
 
 function initializePage() {
-    $('#myRange').mousemove(function(){
-        $('#rangeValue').text($('#myRange').val());
+    // $('#myRange').mousemove(function(){
+    //     $('#rangeValue').text($('#myRange').val());
+    // });
+    $('#progressbar1').LineProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#3DA67D',
+        height: '20px',
+        radius: '15px',
+        width: '50%'
+    });
+
+    $(document).on("click", "#flag-btn-1", function(event){
+        $('#modal1').modal('show');
+    });
+
+    $(document).on("click", ".close-btn-1", function(event){
+        $('#modal1').modal('toggle');
+    });
+
+    $(document).on("click", "#flag-btn-2", function(event){
+        $('#modal2').modal('show');
+    });
+
+    $(document).on("click", ".close-btn-2", function(event){
+        $('#modal2').modal('toggle');
+    });
+
+    $(document).on("click", "#flag-btn-3", function(event){
+        $('#modal3').modal('show');
+    });
+
+    $(document).on("click", ".close-btn-3", function(event){
+        $('#modal3').modal('toggle');
+    });
+
+    $(document).on("click", "#cap-btn", function(event){
+        $('#modal4').modal('show');
+    });
+
+    $(document).on("click", ".close-btn-4", function(event){
+        $('#modal4').modal('toggle');
     });
 }
